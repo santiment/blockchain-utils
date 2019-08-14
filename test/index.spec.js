e604ae9099164cce59ace0473572ef8973a118e1
Valentin Mihov <valentin.mihov@gmail.com>
2019-08-14 16:30:54 +0300

    Added a function that filters out error traces in ETH transactions
    Filtering the traces with errors could be a non-trivial task for
    partially executed ETH transactions.

---
+++ test/index.spec.js
@@ -0,0 +1,23 @@
+const assert = require("assert")
+
+const { filterErrors } = require('../lib/eth')
+const testErrTxs = require('./test_err_txs.json')
+const testParentErr = require('./test_parent_err.json')
+
+describe('Find errors', function () {
+  it('Find hashes of all txs with errors', function () {
+    assert.strictEqual(
+      filterErrors(testErrTxs).length,
+      1
+    )
+  })
+})
+
+describe('Find parent errors', function () {
+  it('Find children of trace with error', function () {
+    assert.strictEqual(
+      filterErrors(testParentErr).length,
+      8
+    )
+  })
+})
